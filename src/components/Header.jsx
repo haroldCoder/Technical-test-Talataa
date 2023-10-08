a0f9462d66814383976b41864d42694d8d5742e3
haroldCoder <haroldc2005@gmail.com>
2023-10-08 17:24:52 -0500

    terminate header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import coder from '../assets/coder.png'
 
 export const Header = () => {
   return (
     <div className='header p-4'>
         <h1>Employee Talataa</h1>
+        <img src={coder} alt='coder' />
     </div>
   )
 }
