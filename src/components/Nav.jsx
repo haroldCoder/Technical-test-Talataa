493bb8f7cbddd500b527a942789786612453b60a
haroldCoder <haroldc2005@gmail.com>
2023-10-08 17:25:26 -0500

    create navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <h2>Home</h2>
+        <h2>Add new user</h2>
+        <h2>support creator</h2>
+    </div>
+  )
+}
+
+export default Nav
