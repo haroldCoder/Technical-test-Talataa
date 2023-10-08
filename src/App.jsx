ad8caf82ebaf215977bd408ccf225ab6e11fed91
haroldCoder <haroldc2005@gmail.com>
2023-10-08 17:26:25 -0500

    view changes and ignore .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import { Header } from './components/Header'
+import Nav from './components/Nav'
+import $ from "jquery";
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
+  useEffect(()=>{
+      $(".nav > h2").hover(function(){
+        $(this).css("color", "#2AABFF");
+      }, function(){
+        $(this).css("color", "#444");
+      });
+      $(".nav > h2").css("cursor", "pointer");
+  },[])
+
   return (
-    <Header />
+    <>
+      <Header />
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
